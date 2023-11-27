1228202cf5814f212f816533fea89d4b391d0a6f
asanorberg <145440223+asanorberg@users.noreply.github.com>
2023-11-27 12:13:01 +0100

    first commit

---
+++ question4.js
@@ -0,0 +1,29 @@
+// 4. Shoppingcart med separata priser (3p)
+// Skriv klart funktionen calculateTotalPrice2 som tar en array med produkter
+// och ett objekt med priser som input och returnerar det totala priset
+
+const modelPrices = {
+  EOS_70D: 100,
+  D3400: 120,
+  Hero_4: 80,
+  Phantom: 50,
+  Karma: 200,
+};
+
+const shoppingCart = [
+  { name: "Camera", brand: "Canon", model: "EOS_70D" },
+  { name: "Camera", brand: "Nikon", model: "D3400" },
+  { name: "Camera", brand: "GoPro", model: "Hero_4" },
+  { name: "Drone", brand: "DJI", model: "Phantom" },
+  { name: "Drone", brand: "GoPro", model: "Karma" },
+];
+
+const modelinCart = shoppingCart.map((item) => {
+  return item.model;
+  console.log(modelinCart);
+});
+
+function calculateTotalPrice2(shoppingCart, prices) {}
+//Här blev det hjärnsläpp tyvärr :(
+
+console.log(calculateTotalPrice2(shoppingCart, modelPrices)); // Ska logga 550
